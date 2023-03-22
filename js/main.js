8e05c267db1bfd56bf7f590aa83ff14118a9b9af
Fatumxxx <vasylzaya@gmail.com>
2023-03-22 18:07:30 +0200

    create project

---
+++ js/main.js
@@ -0,0 +1,144 @@
+
+$(function () {
+    // change header on scroll --------------
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".header").addClass("header-active");
+        } else {
+            $(".header").removeClass("header-active");
+        }
+    });
+
+    //  BURGER MENU -------------------------------- //
+    $('.burger-btn').on('click', function () {
+        $(this).toggleClass('active');
+        $('.header__menu').toggleClass('active');
+    });
+    $('.header__menu').find('li').on('click', function () {
+        $('.burger-btn').removeClass('active');
+        $('.header__menu').removeClass('active');
+    });
+
+    // POPUP ---------------------------------------------- //
+
+    $('.popup_btn').on('click', function () {
+        var indexPopup = $(this).attr('data-popup');
+        $('.popup__window').removeClass('active');
+        $('.' + indexPopup).addClass('active');
+        return false;
+    });
+    $('.popup__close').on('click', function () {
+        $('.popup__window').removeClass('active');
+    });
+
+
+
+    //  padding on full width blocks---------------------- //
+    let windowWidth = $(window).width(),
+        containerWidth = $('.container').width(),
+        sideWidth = (windowWidth - containerWidth) / 2;
+
+    $('.right-padding').css('padding-right', sideWidth + 'px');
+    $('.left-padding').css('padding-left', sideWidth + 'px');
+
+    $(window).resize(function () {
+        let windowWidth = $(window).width(),
+            containerWidth = $('.container').width(),
+            sideWidth = (windowWidth - containerWidth) / 2;
+
+        $('.right-padding').css('padding-right', sideWidth + 'px');
+        $('.left-padding').css('padding-left', sideWidth + 'px');
+    });
+
+    // products slider --------------------------------- //
+    $('.products__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 500,
+        // cssEase: 'linear',
+        // autoplay: true,
+        // autoplaySpeed: 700,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2.5,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    infinite: false
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    infinite: false
+                }
+            }
+        ]
+    });
+
+    // partners slider --------------------------------- //
+    $('.partners__slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 2500,
+        cssEase: 'linear',
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        swipe: false,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        ]
+    });
+
+    $('.partners__slider a').click(function (e) {
+        e.preventDefault();
+    });
+
+    //  INPUT NUMBER MASK ------------------------ //
+    if ($(".phoneinp").length) {
+        $('.phoneinp').inputmask({
+            "mask": "+38 ( 9 9 9 ) - 9 9 9 - 9 9 - 9 9",
+            showMaskOnHover: false,
+            showMaskOnFocus: true,
+        });
+    }
+
+});
